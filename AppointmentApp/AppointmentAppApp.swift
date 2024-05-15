4840f2ad7ae05873c6745594950b5ec00913cae8
William Zhang <zhengwu1117@gmail.com>
2024-05-15 14:32:35 -0500

    Initial Commit

---
+++ AppointmentApp/AppointmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppointmentAppApp.swift
+//  AppointmentApp
+//
+//  Created by William Zhang on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AppointmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
